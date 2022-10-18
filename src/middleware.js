1cd0573d30910b2e0f1aca0726aa03878aeab7ea
bernadettechrestella <bernadettechrestella@gmail.com>
2022-10-18 11:39:06 +0800

    Import Login view

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import App from './App'
+import Login from './components/login';
 
 const Middleware = () => {
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<App />}/>
+                <Route path="/loginAdmin" element={<Login />}/>
             </Routes>
         </Router>
     )
